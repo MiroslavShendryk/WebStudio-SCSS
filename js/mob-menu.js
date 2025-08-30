069c0c3ec6272268cec260c7e4a15c3d29ba72ba
MyroslavShendrik <citrus.user2204@gmail.com>
2025-08-30 21:04:07 +0300

    test

--- js/mob-menu.js
+++ js/mob-menu.js
@@ -1,14 +1,22 @@
 (() => {
-    const refs = {
-      openMenuBtn: document.querySelector('.mob-menu-open-btn'),
-      closeMenuBtn: document.querySelector('.mob-menu-close-btn'),
-      menu: document.querySelector('.mob-menu-box'),
-    };
-  
-    refs.openMenuBtn.addEventListener('click', toggleMenu);
-    refs.closeMenuBtn.addEventListener('click', toggleMenu);
-  
-    function toggleMenu() {
+  const refs = {
+    openMenuBtn: document.querySelector('.mob-menu-open-btn'),
+    closeMenuBtn: document.querySelector('.mob-menu-close-btn'),
+    menu: document.querySelector('.mob-menu-box'),
+  };
+
+  function toggleMenu() {
+    if (window.innerWidth <= 480) {
       refs.menu.classList.toggle('is-hidden');
     }
-  })();
+  }
+
+  refs.openMenuBtn.addEventListener('click', toggleMenu);
+  refs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  window.addEventListener('resize', () => {
+    if (window.innerWidth > 480) {
+      refs.menu.classList.add('is-hidden');
+    }
+  });
+})();
